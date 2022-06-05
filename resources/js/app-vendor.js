8d3e660708143c3d9e4b68cc670028c53eaaaa4b
gedeyoga <permana0912@gmail.com>
2022-06-05 20:10:35 +0800

    first commit

---
+++ resources/js/app-vendor.js
@@ -0,0 +1,7 @@
+window.$ = require("jquery");
+window.jQuery = require("jquery"); 
+
+
+
+// require("../template/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars");
+// require("../template/admin-lte/dist/js/adminlte");
